7ced7b9243507c164cb32db7ab20e8566ff95af9
kiga0151 <kiralyvari.gabor@gmail.com>
2021-01-14 12:55:13 +0100

    Add files via upload
    this is an uploaded file

---
+++ types.py
@@ -0,0 +1,13 @@
+a=25
+b=50
+c=25
+
+message = f"az első {1}, a második {b}, a harmadik {c}"
+print(message)
+
+print(6 + 4 / 2 - (1 * 2))
+
+szam = input("mennyi?: ")
+print(type(szam))
+eredmeny = int(szam) / 2
+print(f"az eredmeny {eredmeny}")
